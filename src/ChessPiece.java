fbb79ea4f7e5a6141b5559e01a2b20edc98b5bed
DB Butler <inquisitor314@gmail.com>
2018-01-19 13:30:29 -0700

    Chess Piece headers
    ChessPiece method headers added

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
 
 public abstract class ChessPiece {
+	
+	protected ChessBoard board;
+	protected int row;
+	protected int column;
+	protected Color color;
 
+	public enum Color {
+		WHITE, BLACK;
+	}	
+	
+	public ChessPiece(ChessBoard board, Color color) {
+		this.board = board;
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public String getPosition() {
+		return null;
+	}
+	
+	//throws IllegalPositionException
+	public void setPosition(String position) {
+		
+	}
+	
+	abstract public String toString();
+	
+	abstract public ArrayList<String> legalMoves();
+	
 }
