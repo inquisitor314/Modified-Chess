b0e191fb71ff013ca4836b1c8485aa4fc693fc10
DB Butler <inquisitor314@gmail.com>
2018-01-19 13:17:16 -0700

    ChessBoard method headers
    Method headers added to ChessBoard

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -6,4 +6,35 @@ public class ChessBoard {
 	public ChessBoard() {
 		board = new ChessPiece[8][8];
 	}
+	
+	public void initialize() {
+		
+	}
+	
+	//throws IllegalPositionException
+	public ChessPiece getPiece(String position) {
+		return null;
+	}
+	
+	public boolean placePiece(ChessPiece piece, String position) {
+		return false;
+	}
+	
+	//throws IllegalMoveException
+	public void move(String fromPosition, String toPosition) {
+		
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+	    ChessBoard board = new ChessBoard();
+	    board.initialize();
+	    System.out.println(board);
+	    board.move("c2", "c4");
+	    System.out.println(board);
+	}
+	
 }
