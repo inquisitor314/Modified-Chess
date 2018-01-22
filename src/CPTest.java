8614b49fa2ec5019e991e7cfbca763aaa7b085bb
DB Butler <inquisitor314@gmail.com>
2018-01-22 16:08:45 -0700

    Chess Piece Test
    Added jUnit test class for ChessPiece.

---
+++ src/CPTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CPTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testChessPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLegalMoves() {
+		fail("Not yet implemented");
+	}
+
+}
