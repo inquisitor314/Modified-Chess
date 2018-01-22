79502e963fc0270252b1101b6e0d94e61feb043c
DB Butler <inquisitor314@gmail.com>
2018-01-22 16:07:18 -0700

    ChessBoard Test
    added jUnit test class for ChessBoard

---
+++ src/CBTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CBTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testChessBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlacePiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
